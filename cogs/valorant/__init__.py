4cc1c48cf28b4c511dbd3f14cc582bdc96052316
STACIA <69411527+staciax@users.noreply.github.com>
2023-04-21 00:28:50 +0700

    Update __init__.py

--- cogs/valorant/__init__.py
+++ cogs/valorant/__init__.py
@@ -1,21 +1,27 @@
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
 from .main import Valorant
 
+_log = logging.getLogger(__name__)
+
+skip = False
 try:
     import bs4  # type: ignore
     import lxml  # type: ignore
 except ImportError:
-    raise RuntimeError(
+    skip = True
+    _log.warning(
         "bs4 and lxml are not installed. Please install them with `pip3 install -r cogs/valorant/requirements.txt`."
     )
-
 
 if TYPE_CHECKING:
     from core.bot import LatteMaid
 
 
 async def setup(bot: LatteMaid) -> None:
-    await bot.add_cog(Valorant(bot))
+    if not skip:
+        await bot.add_cog(Valorant(bot))
+        return
