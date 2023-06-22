27a2d78da52dbb2e83a1629298bc1b1622d40a61
STACIA <69411527+staciax@users.noreply.github.com>
2023-06-23 02:18:20 +0700

    error handlers

--- cogs/valorant/error.py
+++ cogs/valorant/error.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 import discord
 from discord import app_commands
 
+import valorantx2 as valorantx
+
+# from core.errors import UserInputError
 from core.i18n import I18n
+
+# from core.ui.embed import MiadEmbed as Embed
+from valorantx2 import valorant_api
 
 from .abc import MixinMeta
 
@@ -23,6 +29,51 @@ class ErrorHandler(MixinMeta):
     async def cog_app_command_error(
         self,
         interaction: discord.Interaction[LatteMaid],
-        error: app_commands.AppCommandError,
+        error: Union[app_commands.AppCommandError, Exception],
     ) -> None:
-        await super().cog_app_command_error(interaction, error)  # type: ignore
+        locale = interaction.locale
+        # embed = Embed().error()
+
+        message = _('An error occurred while processing the command.', locale)
+
+        if isinstance(error, app_commands.errors.CommandInvokeError):
+            error = error.original
+
+        if not isinstance(error, (valorant_api.errors.ValorantAPIError, valorantx.errors.ValorantXError)):
+            self.bot.dispatch('app_command_error', interaction, error)
+            # return to global error handler
+            return
+
+        # valorant http error
+        if isinstance(error, valorantx.errors.InGameAPIError):
+            # valorant in game api error
+            if isinstance(error, valorantx.errors.BadRequest):  # status code 400
+                message = _(f'Bad Request from Riot API\n{error.text}', locale)
+            elif isinstance(error, valorantx.errors.Forbidden):  # status code 403
+                message = _(f'Forbidden from Riot API \n{error.text}', locale)
+            elif isinstance(error, valorantx.errors.NotFound):  # status code 404
+                message = _(f'Not Found from Riot API \n{error.text}', locale)
+            elif isinstance(error, valorantx.errors.RateLimited):  # status code 429
+                message = _(f'You are rate limited from Riot API \n{error.text}', locale)
+            elif isinstance(error, valorantx.errors.InternalServerError):  # status code 500
+                message = _(f'Internal Server Error from Riot API \n{error.text}', locale)
+
+        # auth error
+        elif isinstance(error, valorantx.errors.RiotAuthError):
+            if isinstance(error, valorantx.errors.RiotAuthenticationError):
+                message = _('Invalid username or password.', locale)  # 'Invalid username or password.'
+            elif isinstance(error, (valorantx.errors.RiotRatelimitError, valorantx.errors.RiotAuthRateLimitedError)):
+                message = _('Riot Rate Limit Error', locale)
+            elif isinstance(
+                error, (valorantx.errors.RiotUnknownResponseTypeError, valorantx.errors.RiotUnknownErrorTypeError)
+            ):
+                message = _('Riot Unknown Error', locale)
+
+        # valorant api error
+        # elif isinstance(error, valorant_api.errors.ValorantAPIError):
+        #     ...
+
+        # raise UserInputError(message)
+
+        # interaction.extras['embed'] = embed
+        self.bot.dispatch('app_command_error', interaction, error)
