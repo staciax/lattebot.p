fe60373008e90b29cde334a33e05031919f830f5
STACIA <69411527+staciax@users.noreply.github.com>
2023-06-24 01:42:50 +0700

    remove get_store_storefront method

--- valorantx2/http.py
+++ valorantx2/http.py
@@ -1,22 +1,20 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Coroutine, Optional, TypeVar
+from typing import TYPE_CHECKING
 
-from valorantx import Region
-from valorantx.http import HTTPClient as ValorantXHTTPClient, Route as ValorantXRoute
+from valorantx.enums import Region
+from valorantx.http import HTTPClient as _HTTPClient, Route
 
 from .auth import RiotAuth
 
 if TYPE_CHECKING:
     from asyncio import AbstractEventLoop
 
-    from valorantx.types import store
+    from valorantx.http import Response
 
     from .auth import RiotAuth
     from .types import account_henrikdev
 
-    T = TypeVar('T')
-    Response = Coroutine[Any, Any, T]
 
 # fmt: off
 __all__ = (
@@ -25,25 +23,20 @@ __all__ = (
 # fmt: on
 
 
-class HTTPClient(ValorantXHTTPClient):
+class HTTPClient(_HTTPClient):
     def __init__(self, loop: AbstractEventLoop) -> None:
         super().__init__(loop, region=Region.AsiaPacific)  # default is AsiaPacific
-        self.riot_auth: RiotAuth = RiotAuth()
+        self.riot_auth: RiotAuth = RiotAuth()  # set riot auth to default
 
     async def re_build_headers(self) -> None:
         self._puuid = self.riot_auth.puuid
         await self.__build_headers()
 
     def get_partial_account(self, game_name: str, tag_line: str) -> Response[account_henrikdev.Response]:
-        r = ValorantXRoute.from_url(
+        r = Route.from_url(
             'GET',
             'https://api.henrikdev.xyz/valorant/v1/account/{game_name}/{tag_line}',
             game_name=game_name,
             tag_line=tag_line,
         )
         return self.request(r, headers={})
-
-    def get_store_storefront(self, puuid: Optional[str] = None) -> Response[store.StoreFront]:
-        puuid = puuid or self.puuid
-        # headers = {}
-        return self.request(ValorantXRoute('GET', '/store/v2/storefront/{puuid}', self.region, puuid=self.puuid))
