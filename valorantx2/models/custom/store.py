12ec3c0af38ff81e2bc20f208d4c850354d2eda2
STACIA <69411527+staciax@users.noreply.github.com>
2023-07-01 04:22:28 +0700

    add AgentStore

---
+++ valorantx2/models/custom/store.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from valorantx.models.contracts import RecruitmentProgressUpdate
+from valorantx.models.store import AgentStore as _AgentStore
+
+if TYPE_CHECKING:
+    from ...auth import RiotAuth
+    from ...client import Client
+
+
+class AgentStore(_AgentStore):
+    if TYPE_CHECKING:
+        _client: Client
+
+    async def fetch_featured_agent_recruitment_progress(
+        self, riot_auth: Optional[RiotAuth]
+    ) -> Optional[RecruitmentProgressUpdate]:
+        contracts = await self._client.fetch_contracts(riot_auth=riot_auth)
+        for processed_match in sorted(
+            contracts.processed_matches,
+            key=lambda x: x.recruitment_progress_update.progress_after if x.recruitment_progress_update else -1,
+            reverse=True,
+        ):
+            if processed_match.recruitment_progress_update is None:
+                continue
+            if processed_match.recruitment_progress_update.group_id == self.featured_agent_id:
+                return processed_match.recruitment_progress_update
+        return None
