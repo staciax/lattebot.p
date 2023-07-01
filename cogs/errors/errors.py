8e2eb196d67724e6cecdfa581952487e7cb8ef52
STACIA <69411527+staciax@users.noreply.github.com>
2023-07-01 22:49:22 +0700

    improvement errors

--- cogs/errors/errors.py
+++ cogs/errors/errors.py
@@ -4,7 +4,7 @@ import contextlib
 import io
 import logging
 import traceback
-from typing import TYPE_CHECKING, Optional, Union
+from typing import TYPE_CHECKING, Optional, Tuple, Union
 
 import discord
 from discord import app_commands, ui
@@ -15,6 +15,7 @@ from core.cog import Cog
 from core.i18n import I18n, cog_i18n
 from core.ui.embed import MiadEmbed
 from core.ui.views import BaseView
+from core.utils.chat_formatting import code_block
 
 if TYPE_CHECKING:
     from discord.ui import Item, Modal
@@ -34,10 +35,10 @@ async def application_error_handler(
     interaction: discord.Interaction[LatteMaid],
     error: Union[Exception, app_commands.AppCommandError],
 ) -> None:
-    client = interaction.client
     locale = interaction.locale
+    title, message = get_error_handle_message(error, locale)  # interaction.extras.get('embed')
+    embed = get_error_handle_embed(interaction.user, title, message)
 
-    embed = build_error_handle_embed(interaction.user, error, locale)  # interaction.extras.get('embed')
     view = guild_support_view(locale)
 
     with contextlib.suppress(discord.HTTPException):
@@ -59,44 +60,18 @@ async def application_error_handler(
             # kwargs['delete_after'] = 60
             await interaction.response.send_message(**kwargs)
 
-        _log_error(interaction, error)
-
-        if client.traceback_log is not None and embed.custom_id is not None and embed.custom_id == 'traceback':
-            traceback_formatted = f"```py\n{traceback.format_exc()}\n```"
-            if len(traceback_formatted) >= 1980:
-                paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1980)
-                result = str(traceback.format_exc())
-                if len(result) <= 2000:
-                    if result.strip() == '':
-                        result = '\u200b'
-                paginator.add_line(result)
-                interface = PaginatorInterface(
-                    client,
-                    paginator,
-                    owner=client.owner,
-                    emoji='<:ThinkO_O:744344862521950268>',
-                )
-                client.loop.create_task(interface.send_to(client.traceback_log))
-            else:
-                fp = io.BytesIO(traceback.format_exc().encode('utf-8'))
-                traceback_fp = discord.File(fp, filename='traceback.py')
-                client.loop.create_task(client.traceback_log.send(content=client.owner.mention, file=traceback_fp))
-
 
 def guild_support_view(locale: discord.Locale) -> ui.View:
     view = BaseView().url_button(_('Support Server', locale), 'https://discord.gg/4N2YkXbM')
     return view
 
 
-def build_error_handle_embed(
-    user: Optional[Union[discord.User, discord.Member]],
-    error: Exception,
-    locale: discord.Locale,
-) -> MiadEmbed:
+def get_error_handle_message(error: Exception, locale: discord.Locale) -> Tuple[str, str]:
     # item = interaction.extras.get('item')
     # modal = interaction.extras.get('modal')
 
-    embed = MiadEmbed().error()
+    title = _('Error', locale)
+    message = _('An unknown error occurred.', locale)
 
     if isinstance(error, app_commands.errors.CommandInvokeError):
         error = error.original
@@ -107,51 +82,71 @@ def build_error_handle_embed(
         10008,  # Unknown message
         10015,  # Unknown webhook
     ):
-        embed.description = _('The message was deleted.', locale)
+        title = _('Not Found', locale)
+        message = _('The message was deleted.', locale)
     elif isinstance(error, errors.ComponentOnCooldown):
-        embed.description = _('You are on cooldown. Please try again in {seconds:.2f} seconds.', locale).format(
+        title = _('Cooldown', locale)
+        message = _('You are on cooldown. Please try again in {seconds:.2f} seconds.', locale).format(
             seconds=error.retry_after
         )
     elif isinstance(error, errors.UserInputError):
-        embed.description = error.message
+        message = error.message
     elif isinstance(error, errors.CheckFailure):
-        # command = error.command
-        # author = error.author
-        embed.description = error.message
+        title = _('Check Failure', locale)
+        command = error.command
+        author = error.author
+        fmt = 'Only {author} can use this command. If you want to use it, use {command}'
+        if isinstance(command, (app_commands.Command, app_commands.ContextMenu)) and author is not None:
+            message = fmt.format(author=author.mention, command=command.qualified_name)
+        elif isinstance(command, (app_commands.AppCommand, app_commands.AppCommandGroup)) and author is not None:
+            message = fmt.format(author=author.mention, command=command.mention)
+        else:
+            message = _('You are not allowed to use this.', locale)
     elif isinstance(error, app_commands.errors.AppCommandError):
         if isinstance(error, app_commands.errors.CommandOnCooldown):
-            embed.description = _('You are on cooldown. Please try again in {seconds:.2f} seconds.', locale).format(
+            title = _('Cooldown', locale)
+            message = _('You are on cooldown. Please try again in {seconds:.2f} seconds.', locale).format(
                 seconds=error.retry_after
             )
         # elif isinstance(error, (app_commands.errors.MissingRole, app_commands.errors.MissingAnyRole)):
-        #     embed.description = _('You do not have the required role(s).', 0, locale)
+        #     message = _('You do not have the required role(s).', locale)
         elif isinstance(error, app_commands.errors.CommandNotFound):
-            embed.description = _(f'CommandNotFound', locale)
+            title = _('Command Not Found', locale)
+            message = _(f'CommandNotFound', locale)
         elif isinstance(error, app_commands.errors.NoPrivateMessage):
-            embed.description = _('UserAlreadyExists', locale)
+            title = _('No Private Message', locale)
+            message = _('UserAlreadyExists', locale)
     elif isinstance(error, database.errors.DatabaseBaseError):
         if isinstance(error, database.errors.UserAlreadyExists):
-            embed.description = _('You are already registered.', locale)
+            title = _('User Already Exists', locale)
+            message = _('You are already registered.', locale)
         elif isinstance(error, database.errors.UserDoesNotExist):
-            embed.description = _('You are not registered.', locale)
+            title = _('User Does Not Exist', locale)
+            message = _('You are not registered.', locale)
         elif isinstance(error, database.errors.BlacklistAlreadyExists):
-            embed.description = _('You are already blacklisted.', locale)
+            title = _('Blacklist Already Exists', locale)
+            message = _('You are already blacklisted.', locale)
         elif isinstance(error, database.errors.BlacklistDoesNotExist):
-            embed.description = _('You are not blacklisted.', locale)
+            title = _('Blacklist Does Not Exist', locale)
+            message = _('You are not blacklisted.', locale)
         elif isinstance(error, database.errors.RiotAccountAlreadyExists):
-            embed.description = _('The Riot account is already registered.', locale)
+            title = _('Riot Account Already Exists', locale)
+            message = _('The Riot account is already registered.', locale)
         elif isinstance(error, database.errors.RiotAccountDoesNotExist):
-            embed.description = _('The Riot account is not registered.', locale)
-    else:
-        embed.description = _('An unknown error occurred.', locale)
-        # embed.custom_id = 'traceback'
+            title = _('Riot Account Does Not Exist', locale)
+            message = _('The Riot account is not registered.', locale)
 
-    # except aiohttp.ClientResponseError as e:
-    #     _log.error('Riot server is currently unavailable.', exc_info=e)
-    #     raise UserInputError(_('Riot server is currently unavailable.', interaction.locale)) from e
+    return title, message
 
+
+def get_error_handle_embed(
+    user: Optional[Union[discord.User, discord.Member]],
+    title: str,
+    message: str,
+) -> MiadEmbed:
+    embed = MiadEmbed(description=message).error()
     icon_url = user.display_avatar.url if user else None
-    embed.set_author(name=embed.author.name or 'Error', icon_url=icon_url)
+    embed.set_author(name=title, icon_url=icon_url)
     return embed
 
 
@@ -165,28 +160,6 @@ def _log_error(interaction: discord.Interaction[LatteMaid], error: Exception) ->
     else:
         _log.error('exception ', exc_info=error)
 
-    # traceback_formatted = f"```py\n{traceback.format_exc()}\n```"
-    # fp = io.BytesIO(traceback.format_exc().encode('utf-8'))
-    # traceback_fp = discord.File(fp, filename='traceback.py')
-    # if len(traceback_formatted) >= 1980:
-    #     paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1980)
-    #     result = str(traceback.format_exc())
-    #     if len(result) <= 2000:
-    #         if result.strip() == '':
-    #             result = "\u200b"
-    #     paginator.add_line(result)
-    #     interface = PaginatorInterface(
-    #         interaction.client, paginator, owner=interaction.user, emoji='<:ThinkO_O:744344862521950268>'
-    #     )
-    # await interaction.client.traceback_log.send(embed=embed, file=traceback_fp)
-    # interaction.client.loop.create_task(interface.send_to(interaction.client.owner))
-    # else:
-    # embed.custom_id = 'traceback'
-    # await self.bot.traceback_log.send(embed=embed, file=traceback_fp)
-    # embed.description = f"```{type(e)}: {e}```"
-    # embed.set_author(name=text_map.get(135, lang))
-    # embed.set_thumbnail(url="https://i.imgur.com/Xi51hSe.gif")
-
 
 @cog_i18n(_)
 class Errors(Cog, name='errors'):
@@ -195,18 +168,44 @@ class Errors(Cog, name='errors'):
     def __init__(self, bot: LatteMaid) -> None:
         self.bot = bot
 
+    async def send_traceback(self, interaction: discord.Interaction[LatteMaid]) -> None:
+        embed = MiadEmbed(timestamp=interaction.created_at).error()
+        embed.set_author(name=f'{interaction.user} | {interaction.user.id}', icon_url=interaction.user.avatar)
+
+        traceback_fmt = code_block(traceback.format_exc(), 'py')
+
+        fp = io.BytesIO(traceback.format_exc().encode('utf-8'))
+        traceback_fp = discord.File(fp, filename='traceback.py')
+
+        if len(traceback_fmt) >= 1980:
+            paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1980)
+            result = str(traceback.format_exc())
+            if len(result) <= 2000:
+                if result.strip() == '':
+                    result = '\u200b'
+            paginator.add_line(result)
+            interface = PaginatorInterface(self.bot, paginator, owner=self.bot.owner, emoji='<:ThinkO_O:744344862521950268>')
+            await interface.send_to(self.bot.owner)
+
+        if self.bot.traceback_log is not None:
+            await self.bot.traceback_log.send(embed=embed, file=traceback_fp)
+
     @Cog.listener('on_app_command_error')
     async def on_app_command_error(
         self, interaction: discord.Interaction[LatteMaid], error: Union[Exception, app_commands.errors.AppCommandError]
     ) -> None:
         await application_error_handler(interaction, error)
+        _log_error(interaction, error)
+        # self.bot.loop.create_task(self.send_traceback(interaction))
 
     @Cog.listener('on_view_error')
     async def on_view_error(self, interaction: discord.Interaction[LatteMaid], error: Exception, item: Item) -> None:
         interaction.extras['item'] = item
         await application_error_handler(interaction, error)
+        _log_error(interaction, error)
 
     @Cog.listener('on_modal_error')
     async def on_modal_error(self, interaction: discord.Interaction[LatteMaid], error: Exception, modal: Modal) -> None:
         interaction.extras['modal'] = modal
         await application_error_handler(interaction, error)
+        _log_error(interaction, error)
