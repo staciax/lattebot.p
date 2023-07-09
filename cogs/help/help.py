bacaa2262fcf7a68150574739763e9834764b6c7
STACIA <69411527+staciax@users.noreply.github.com>
2023-07-09 14:59:52 +0700

    fixes

--- cogs/help/help.py
+++ cogs/help/help.py
@@ -86,9 +86,9 @@ class CogButton(ui.Button['HelpCommand']):
 
         max_pages = self.view.source.get_max_pages()
         if max_pages is not None and max_pages > 1:
-            self.view.add_nav_buttons()
+            self.view._add_nav_buttons()
         else:
-            self.view.remove_nav_buttons()
+            self.view._remove_nav_buttons()
 
         self.disabled = True
         for child in self.view.children:
@@ -99,11 +99,11 @@ class CogButton(ui.Button['HelpCommand']):
 
 
 class HelpCommand(ViewAuthor, LattePages):
-    def __init__(self, interaction: discord.Interaction[LatteMaid], cogs: list[str]) -> None:
+    def __init__(self, interaction: discord.Interaction[LatteMaid], cogs: tuple[str, ...]) -> None:
         super().__init__(interaction, timeout=600.0)
         self.cogs = cogs
-        self.embed: Embed = self.front_help_command_embed(interaction)
-        self.home_button.emoji = self.bot.emoji.latte_icon
+        self.embed: Embed = self._front_help_command_embed(interaction)
+        # self.home_button.emoji = self.bot.emoji.latte_icon
         self.go_to_last_page.row = 1
         self.go_to_first_page.row = 1
         self.go_to_previous_page.row = 1
@@ -111,12 +111,7 @@ class HelpCommand(ViewAuthor, LattePages):
         self.cooldown = commands.CooldownMapping.from_cooldown(5.0, 15.0, user_check)
         self.clear_items()
 
-    def _update_labels(self, page_number: int) -> None:
-        super()._update_labels(page_number)
-        self.go_to_next_page.label = 'next'
-        self.go_to_previous_page.label = 'prev'
-
-    def front_help_command_embed(self, interaction: discord.Interaction[LatteMaid]) -> Embed:
+    def _front_help_command_embed(self, interaction: discord.Interaction[LatteMaid]) -> Embed:
         assert self.bot.user is not None
         embed = Embed(timestamp=interaction.created_at).white()
         embed.set_author(
@@ -126,30 +121,19 @@ class HelpCommand(ViewAuthor, LattePages):
         embed.set_image(url='https://cdn.discordapp.com/attachments/1001848697316987009/1001848873385472070/help_banner.png')
         return embed
 
-    @ui.button(emoji='🏘️', style=discord.ButtonStyle.primary, disabled=True)
-    async def home_button(self, interaction: discord.Interaction[LatteMaid], button: ui.Button) -> None:
-        await interaction.response.defer()
-        self.home_button.disabled = True
-        for child in self.children:
-            if isinstance(child, CogButton):
-                child.disabled = False
-        self.remove_nav_buttons()
-        if self.message is not None:
-            await self.message.edit(embed=self.embed, view=self)
-
-    def add_nav_buttons(self) -> None:
+    def _add_nav_buttons(self) -> None:
         self.add_item(self.go_to_first_page)
         self.add_item(self.go_to_previous_page)
         self.add_item(self.go_to_next_page)
         self.add_item(self.go_to_last_page)
 
-    def remove_nav_buttons(self) -> None:
+    def _remove_nav_buttons(self) -> None:
         self.remove_item(self.go_to_first_page)
         self.remove_item(self.go_to_previous_page)
         self.remove_item(self.go_to_next_page)
         self.remove_item(self.go_to_last_page)
 
-    def add_cog_buttons(self) -> None:
+    def _add_cog_buttons(self) -> None:
         for cog in sorted(self.bot.cogs.values(), key=lambda c: c.qualified_name.lower()):
             if cog.qualified_name.lower() not in self.cogs:
                 continue
@@ -157,11 +141,39 @@ class HelpCommand(ViewAuthor, LattePages):
                 continue
             self.add_item(CogButton(cog=cog))
 
+    def _update_labels(self, page_number: int) -> None:
+        super()._update_labels(page_number)
+        self.go_to_next_page.label = 'next'
+        self.go_to_previous_page.label = 'prev'
+        # TODO: i18n in lattepages class
+
+    async def before_callback(self, interaction: discord.Interaction[LatteMaid]) -> None:
+        if self.locale == interaction.locale:
+            return
+
+        self.locale = interaction.locale
+        self.embed = self._front_help_command_embed(interaction)
+
     async def callback(self) -> None:
         self.add_item(self.home_button)
-        self.add_cog_buttons()
+        self._add_cog_buttons()
         await self.interaction.response.send_message(embed=self.embed, view=self)
         self.message = await self.interaction.original_response()
+
+    @ui.button(emoji='🏘️', style=discord.ButtonStyle.primary, disabled=True)
+    async def home_button(self, interaction: discord.Interaction[LatteMaid], button: ui.Button) -> None:
+        # disable home button
+        button.disabled = True
+
+        # disable all cog buttons
+        for child in self.children:
+            if isinstance(child, CogButton):
+                child.disabled = False
+
+        # remove nav buttons
+        self._remove_nav_buttons()
+
+        await interaction.response.edit_message(embed=self.embed, view=self)
 
 
 @cog_i18n(_)
@@ -175,7 +187,7 @@ class Help(Cog, name='help'):
     @bot_has_permissions(send_messages=True, embed_links=True)
     @dynamic_cooldown(cooldown_short)
     async def help_command(self, interaction: discord.Interaction[LatteMaid]):
-        cogs = ['about', 'valorant']
+        cogs = ('about', 'valorant')
         help_command = HelpCommand(interaction, cogs)
         await help_command.callback()
 
