84afa7cfe2bb5ae7e4bb0da104a00b559131c526
STACIA <69411527+staciax@users.noreply.github.com>
2023-07-10 12:08:28 +0700

    add validate_riot_id method

--- valorantx2/utils.py
+++ valorantx2/utils.py
@@ -16,3 +16,21 @@ class locale_converter:
     @staticmethod
     def to_discord(locale: ValorantLocale) -> DiscordLocale:
         return getattr(DiscordLocale, locale.name, DiscordLocale.american_english)
+
+
+def validate_riot_id(riot_id: str) -> tuple[str, str]:
+    if '#' not in riot_id:
+        raise ValueError('Invalid Riot ID.')
+
+    game_name, _, tag_line = riot_id.partition('#')
+
+    if not game_name or not tag_line:
+        raise ValueError('Invalid Riot ID.')
+
+    if len(game_name) > 16:
+        raise ValueError('Invalid Riot ID.')
+
+    if len(tag_line) > 5:
+        raise ValueError('Invalid Riot ID.')
+
+    return game_name, tag_line
