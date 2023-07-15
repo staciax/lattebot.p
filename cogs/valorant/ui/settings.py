c929d33255988d1b8ac9864dc89e362cc0733885
staciax <6505001948@rumail.ru.ac.th>
2023-07-15 20:05:04 +0000

    fixed

--- cogs/valorant/ui/settings.py
+++ cogs/valorant/ui/settings.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Any
 
 import discord
 
+from discord import ui
 from core.i18n import I18n
 from core.ui.views import ViewAuthor
 
@@ -17,9 +18,45 @@ _ = I18n('valorant.ui.settings', Path(__file__).resolve().parent, read_only=True
 class SettingsView(ViewAuthor):
     def __init__(self, interaction: discord.Interaction[LatteMaid], *args: Any, **kwargs: Any) -> None:
         super().__init__(interaction, *args, **kwargs)
+        self.fill_items()
 
-    async def _init(self) -> None:
+    def fill_items(self) -> None:
+        self.add_items(
+            LanguageButton(locale=self.locale),
+            NotificationButton(locale=self.locale),
+        )
+
+class LanguageButton(ui.Button['SettingsView']):
+
+    def __init__(self, *, locale: discord.Locale, **kwargs: Any) -> None:
+        super().__init__(
+            label=_('button.language', locale=locale),
+            **kwargs
+        )
+        self.locale = locale
+    
+    async def callback(self, interaction: discord.Interaction[LatteMaid]) -> None:
         ...
 
-    async def start(self) -> None:
+class NotificationButton(ui.Button['SettingsView']):
+
+    def __init__(self, *, locale: discord.Locale, **kwargs: Any) -> None:
+        super().__init__(
+            label=_('button.notification', locale=locale),
+            **kwargs
+        )
+        self.locale = locale
+    
+    async def callback(self, interaction: discord.Interaction[LatteMaid]) -> None:
+        ...
+
+class PreviousButton(ui.Button['SettingsView']):
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(
+            label='<',
+            **kwargs
+        )
+    
+    async def callback(self, interaction: discord.Interaction[LatteMaid]) -> None:
         ...
